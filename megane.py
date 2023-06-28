9d927d46061477d60b605c55135fb87bd8e3355f
Namito Tsuhako <s23016@std.it-collega.ac.jp>
2023-06-28 08:21:29 +0000

    8

--- megane.py
+++ megane.py
@@ -1,6 +1,11 @@
+def f(N, alist):
+    return b[c - 1]
+
+
 N = int(input())
 alist = map(int, input().split()[:N])
 b = [a for a in alist]
 b.sort()
 c = (N + 1) // 2
-print(b[c - 1])
+result = f(N, alist)
+print(result)
