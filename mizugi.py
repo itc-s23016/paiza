9ea3f8cf68a5159703cc3ab82162d386962c9ac4
Namito Tsuhako <s23016@std.it-collega.ac.jp>
2023-06-28 08:34:19 +0000

    11

--- mizugi.py
+++ mizugi.py
@@ -1,6 +1,11 @@
+def f(n, m, s, t):
+    return len(t)
+
+
 n, m = map(int, input().split())
 s, t = [input() for _ in range(2)]
 for a in s:
     if a in t:
         t = t.replace(a, "", 1)
-print(len(t))
+result = f(n, m, s, t)
+print(result)
