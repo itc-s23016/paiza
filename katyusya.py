e649a73641ae48e729e7e5d34aba2c2a72febc09
Namito Tsuhako <s23016@std.it-collega.ac.jp>
2023-06-28 08:40:46 +0000

    13

--- katyusya.py
+++ katyusya.py
@@ -1,4 +1,9 @@
+def f(n, p, m, q):
+    return a + b
+
+
 [[n, p], [m, q]] = [map(int, input().split()) for _ in range(2)]
 a = n * p
 b = q * ((n + (m - 1)) // m)
-print(a + b)
+result = f(n, p, m, q)
+print(result)
