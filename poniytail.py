eec903ea2c59e6ba55d6e4750c562dfd594a0871
Namito Tsuhako <s23016@std.it-collega.ac.jp>
2023-06-28 08:01:56 +0000

    3

--- poniytail.py
+++ poniytail.py
@@ -1,7 +1,9 @@
+def f(matrix):
+    return "OK" if t >= 3 else "NG"
+
+
 matrix = [(d, e) for [d, e] in [input().split() for _ in range(5)]]
 a = list(d == e for d, e in matrix)
 t = a.count(True)
-if t >= 3:
-    print("OK")
-else:
-    print("NG")
+result = f(matrix)
+print(result)
