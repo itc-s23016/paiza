0bca9746a6c5ff310370e5788d2c89035f2b6553
Namito Tsuhako <s23016@std.it-collega.ac.jp>
2023-06-28 08:38:07 +0000

    12

--- maiku.py
+++ maiku.py
@@ -1,7 +1,9 @@
+def f(n, m):
+    return b if m % a == 0 else b + 1
+
+
 [n, m] = [int(input()) for _ in range(2)]
 a = n * 2
 b = m // a
-if m % a == 0:
-    print(b)
-else:
-    print(b + 1)
+result = f(n, m)
+print(result)
